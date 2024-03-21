349454f69b5e51e84194a6a1b969ceb2ff8f1693
Deiiivy <deivypr28@gmail.com>
2024-03-20 20:01:59 -0500

    finish view create

--- task-crud2/obj/Debug/net8.0/task-crud2.AssemblyInfo.cs
+++ task-crud2/obj/Debug/net8.0/task-crud2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-crud2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91fcb38ec44afb0937b32140afe715cf9054852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017bb7f2113d438a60d71421e5eb205f274d80c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-crud2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-crud2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
